93601a30891a08022205dbe8ea3147fdf0354225
Christine <aqui.christine@gmail.com>
2018-08-11 11:23:47 -0400

    :lipstick: adding routs for handlebar files

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -11,4 +11,11 @@ module.exports = function(app) {
     });
   });
   //
+  app.get("/login", function(req, res) {
+    res.render("login");
+  });
+  //
+  app.get("/signup", function(req, res) {
+    res.render("signup");
+  });
 };
