865fd16079034308c213ed293cdbde92aae413ba
Christine <aqui.christine@gmail.com>
2018-08-11 12:17:28 -0400

    :construction: working on handlebars for index page and routing
    adding some styling at the same time using tachyons #20

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -14,8 +14,36 @@ module.exports = function(app) {
   app.get("/login", function(req, res) {
     res.render("login");
   });
-  //
+
   app.get("/signup", function(req, res) {
     res.render("signup");
+  });
+
+  app.get("/search", function(req, res) {
+    res.render("search");
+  });
+
+  app.get("/result", function(req, res) {
+    res.render("result");
+  });
+
+  app.get("/map", function(req, res) {
+    res.render("map");
+  });
+
+  app.get("/profile", function(req, res) {
+    res.render("profile");
+  });
+
+  app.get("/pickup", function(req, res) {
+    res.render("pickup");
+  });
+
+  app.get("/donation", function(req, res) {
+    res.render("donation");
+  });
+
+  app.get("/charity", function(req, res) {
+    res.render("charity");
   });
 };
