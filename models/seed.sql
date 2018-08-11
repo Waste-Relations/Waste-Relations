95dcaddd29b85c1b8d73939b266a985b1a022c8e
Christine <aqui.christine@gmail.com>
2018-08-10 22:58:42 -0400

    added seed file and Jaws config

---
+++ models/seed.sql
@@ -0,0 +1,4 @@
+INSERT INT wastetable (name, category, subCategory)
+VALUES ('Stomer Burger', 'Green Bin','food - meat, fish & seafood'),
+('HamBurger', 'Green Bin','food - meat, fish & seafood'),
+('Stomer Burger', 'Green Bin','food - meat, fish & seafood');
