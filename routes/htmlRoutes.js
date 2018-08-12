403117258aa2300aea50147a756edd11794ffbe7
Chris <aqui.christine@gmail.com>
2018-08-12 11:37:19 -0400

    working on passport

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -1,13 +1,12 @@
-var db = require("../models");
+const db = require("../models");
+// Requiring our custom middleware for checking if a user is logged in
+const isAuthenticated = require("../config/middleware/isAuthenticated");
 
 module.exports = function(app) {
   // Load index page
   app.get("/", function(req, res) {
-    db.WasteItem.findAll({}).then(function(dbExamples) {
-      res.render("index", {
-        msg: "Welcome",
-        examples: dbExamples
-      });
+    db.WasteItem.findAll({}).then(function() {
+      res.render("index");
     });
   });
   //
@@ -20,30 +19,37 @@ module.exports = function(app) {
   });
 
   app.get("/search", function(req, res) {
-    res.render("search");
+    // If the user already has an account send them to the search page
+    req.user ? res.render("search") : res.render("index");
   });
 
   app.get("/result", function(req, res) {
-    res.render("result");
+    // If the user already has an account send them to the result page
+    req.user ? res.render("result") : res.render("index");
   });
 
   app.get("/map", function(req, res) {
-    res.render("map");
+    // If the user already has an account send them to the map page
+    req.user ? res.render("map") : res.render("index");
   });
 
   app.get("/profile", function(req, res) {
-    res.render("profile");
+    // If the user already has an account send them to the profile page
+    req.user ? res.render("profile") : res.render("index");
   });
 
   app.get("/pickup", function(req, res) {
-    res.render("pickup");
+    // If the user already has an account send them to the pickup page
+    req.user ? res.render("pickup") : res.render("index");
   });
 
   app.get("/donation", function(req, res) {
-    res.render("donation");
+    // If the user already has an account send them to the donation page
+    req.user ? res.render("donation") : res.render("index");
   });
 
   app.get("/charity", function(req, res) {
-    res.render("charity");
+    // If the user already has an account send them to the charity page
+    req.user ? res.render("charity") : res.render("index");
   });
 };
