be610f5e98e1c0eed36747b1acfdcf09200f722a
Chris <aqui.christine@gmail.com>
2018-08-14 21:22:07 -0400

    checking testing on ci

--- public/js/additem.js
+++ public/js/additem.js
@@ -1,23 +1,32 @@
 $(document).ready(function() {
-    function titleCase(str) {
-        return str.toLowerCase().split(' ').map(function(word) {
+  function titleCase(str) {
+    return str
+      .toLowerCase()
+      .split(" ")
+      .map(function(word) {
         return word.replace(word[0], word[0].toUpperCase());
-        }).join(' ');
-    }
-    //grab user input from form    
-    $('#submit').on("click", function(e) {
-        event.preventDefault();
-        var $itemname = $('#name').val().trim();
-        var $itemCategory = $('#where').val().trim();
-        var $itemSubCat = $("#how").val().trim();
-        
-        //let's create an object containing our form info
-        var newTrash = {
-            name: $itemname.toUpperCase(),
-            category: titleCase($itemCategory),
-            subCategory: $itemSubCat.toLowerCase(),
-        }
-        $.post("/api/additem", newTrash).then(function() { 
-        });
-    });
+      })
+      .join(" ");
+  }
+  //grab user input from form
+  $("#submit").on("click", function(e) {
+    e.preventDefault();
+    var $itemname = $("#name")
+      .val()
+      .trim();
+    var $itemCategory = $("#where")
+      .val()
+      .trim();
+    var $itemSubCat = $("#how")
+      .val()
+      .trim();
+
+    //let's create an object containing our form info
+    var newTrash = {
+      name: $itemname.toUpperCase(),
+      category: titleCase($itemCategory),
+      subCategory: $itemSubCat.toLowerCase()
+    };
+    $.post("/api/additem", newTrash);
+  });
 });
